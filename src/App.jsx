8e6f93bd5739eddf5584e7c6869200d41bc29606
DoctorDraxter <doctordraxter@gmail.com>
2022-04-29 10:31:43 -0400

    Lazy loading changed from index.jsx to App.jsx in Layout.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import { Layout, NotFound } from './shared/components/index';
+import { Loader, NotFound } from './shared/components/index';
 import { Generator, Generated } from './pages/index';
+
+const Layout = lazy(() => import('./shared/components/Layout/Index'));
 
 const App = () => {
 	return (
 		<Routes>
-			<Route path='/' element={<Layout />}>
+			<Route
+				path='/'
+				element={
+					<Suspense fallback={<Loader />}>
+						<Layout />
+					</Suspense>
+				}
+			>
 				<Route index element={<Generator />} />
 				<Route path='generated' element={<Generated />} />
 				<Route path='*' element={<NotFound />} />
--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,18 @@
-import React, { lazy, Suspense } from 'react';
+import React from 'react';
 import ReactDom from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import { Loader } from './shared/components';
 
-const App = lazy(() => import('./App'));
+import App from './App';
 
 ReactDom.render(
 	<React.StrictMode>
-		<Suspense fallback={<Loader />}>
-			<Provider store={store}>
-				<BrowserRouter>
-					<App />
-				</BrowserRouter>
-			</Provider>
-		</Suspense>
+		<Provider store={store}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</Provider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
