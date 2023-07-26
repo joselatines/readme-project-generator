d5fa7b7d8c93fff6fda58671a7079ed387de1895
Jose Latines <joselatines33@gmail.com>
2023-07-25 23:16:32 -0400

    feat: add full logic app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import GeneratorForm from "./components/forms/GeneratorForm";
+import MarkdownPreview from "./components/forms/MarkdownPreview";
 
 function App() {
 	return (
 		<main className="grid col-span-2">
 			<GeneratorForm />
-			<div>Preview</div>
+			<MarkdownPreview />
 		</main>
 	);
 }
